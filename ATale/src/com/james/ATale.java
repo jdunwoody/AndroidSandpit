faf1f51afb7eb6d6b86adce95b1fae3d79709a4b
James Dunwoody <jamesdunwoody@gmail.com>
2011-02-28 21:51:45 +1100

    Got the buttons working

--- ATale/src/com/james/ATale.java
+++ ATale/src/com/james/ATale.java
@@ -2,10 +2,14 @@ package com.james;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ATale extends Activity {
 	private LinearLayout linearLayout;
@@ -15,17 +19,18 @@ public class ATale extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// setupButtonListener();
-		// setupMainImage();
-
 		setContentView(R.layout.main);
+
+		setupButtonListener();
+		// setupMainImage();
 	}
 
-	// private OnClickListener buttonClickListener = new OnClickListener() {
-	// public void onClick(View v) {
-	// changeImage();
-	// }
-	// };
+	private OnClickListener buttonClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			TextView textView = (TextView) findViewById(R.id.messageView);
+			textView.setText("excellent");
+		}
+	};
 
 	// private View changeImage() {
 	// ImageView mainImage = (ImageView) findViewById(R.id.mainImage);
@@ -43,10 +48,10 @@ public class ATale extends Activity {
 	// }
 	// }
 
-	// private void setupButtonListener() {
-	// Button button = (Button) findViewById(R.id.mainImage);
-	// button.setOnClickListener(buttonClickListener);
-	// }
+	private void setupButtonListener() {
+		Button button = (Button) findViewById(R.id.buttonForward);
+		button.setOnClickListener(buttonClickListener);
+	}
 
 	private void setupMainImage() {
 		linearLayout = new LinearLayout(this);
