4e71039162e11441ca950ef6b5866f1f4e100647
James Dunwoody <jamesdunwoody@gmail.com>
2011-03-13 20:23:50 +1100

    Fixed view to invalidate and show

--- Flock/src/com/james/view/CustomDrawableView.java
+++ Flock/src/com/james/view/CustomDrawableView.java
@@ -3,8 +3,9 @@ package com.james.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Path;
 import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.PathShape;
 import android.graphics.drawable.shapes.Shape;
 import android.util.Log;
 import android.view.View;
@@ -19,27 +20,25 @@ public class CustomDrawableView extends View {
 
         drawable = new ShapeDrawable(createBirdShape());
         drawable.getPaint().setColor(Color.RED);
-        drawable.setBounds(x, y, 100, 100);
     }
 
     public void touched(float newX, float newY) {
         Log.i(VIEW_LOG_TAG, "touched");
         this.x = (int) newX;
         this.y = (int) newY;
-        this.drawable.setBounds(x, y, x * 10, y * 10);
+        this.drawable.setBounds(x - 5, y - 5, x + 5, y + 5);
+        invalidate();
     }
 
     private Shape createBirdShape() {
-        Shape shape = new OvalShape();
-        return shape;
-        // Path path = new Path();
-        // path.lineTo(0, 5);
-        // path.lineTo(5, 0);
-        // path.lineTo(-5, 5);
-        // path.lineTo(0, 5);
-        //
-        // PathShape pathShape = new PathShape(path, 10, 10);
-        // return pathShape;
+        Path path = new Path();
+        path.moveTo(x, y);
+        path.lineTo(x + 5, y + 5);
+        path.lineTo(x - 5, y + 5);
+        path.lineTo(x, y);
+
+        PathShape pathShape = new PathShape(path, 10, 10);
+        return pathShape;
     }
 
     protected void onDraw(Canvas canvas) {
