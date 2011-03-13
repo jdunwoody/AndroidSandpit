14df4cf5f46ffb5ce86e143b34ef14d0b2de9493
James Dunwoody <jamesdunwoody@gmail.com>
2011-03-13 17:26:56 +1100

    Renamed to fingerpaint

---
+++ FingerPaint/src/com/james/fingerpaint/CustomExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.james.fingerpaint;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class CustomExceptionHandler implements UncaughtExceptionHandler {
+    private UncaughtExceptionHandler defaultUEH;
+    private String                   localPath;
+
+    /*
+     * if any of the parameters is null, the respective functionality will not be used
+     */
+    public CustomExceptionHandler(String localPath) {
+        this.localPath = localPath;
+        this.defaultUEH = Thread.getDefaultUncaughtExceptionHandler();
+    }
+
+    public void uncaughtException(Thread t, Throwable e) {
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        final Writer result = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(result);
+        e.printStackTrace(printWriter);
+        String stacktrace = result.toString();
+        printWriter.close();
+        String filename = timestamp + ".stacktrace";
+
+        if (localPath != null) {
+            writeToFile(stacktrace, filename);
+        }
+        defaultUEH.uncaughtException(t, e);
+    }
+
+    private void writeToFile(String stacktrace, String filename) {
+        try {
+            BufferedWriter bos = new BufferedWriter(new FileWriter(localPath + "/" + filename));
+            bos.write(stacktrace);
+            bos.flush();
+            bos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ FingerPaint/src/com/james/fingerpaint/domain/Nodes.java
@@ -0,0 +1,29 @@
+package com.james.fingerpaint.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nodes {
+    private final List<Node> nodes;
+
+    public Nodes() {
+        nodes = new ArrayList<Node>();
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public Node matches(float x, float y) {
+        for (Node node : nodes) {
+            if (node.matches(x, y)) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    public void add(Node last) {
+        this.nodes.add(last);
+    }
+}
