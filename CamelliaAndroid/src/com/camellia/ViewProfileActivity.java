9c6ab69a9ae9309bc4e2fd14a01359cc88892763
James <James>
2011-08-22 12:23:51 +1000

    Adding view profile - incomplete

--- CamelliaAndroid/src/com/camellia/ViewProfileActivity.java
+++ CamelliaAndroid/src/com/camellia/ViewProfileActivity.java
@@ -1,7 +1,36 @@
 package com.camellia;
 
+import static com.camellia.logging.Logging.log;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 public class ViewProfileActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.view_profile);
+	}
+
+	public void gotoBack(View view) {
+		log("gotoBack");
+		finish();
+	}
+
+	public void gotoPreviousProfile(View view) {
+		log("gotoPreviousProfile");
+	}
+
+	public void gotoNextProfile(View view) {
+		log("gotoNextProfile");
+	}
+
+	public void gotoAddProfileProfile(View view) {
+		log("gotoAddProfileProfile");
+	}
+
+	public void gotoMoreProfile(View view) {
+		log("gotoMoreProfile");
+	}
 }
